ab8a90a06587bee50c1c0a0718557bfe901d81ce
a-catarina-lopes <anacatarina.lopes94@gmail.com>
2023-01-18 12:48:39 +0000

    ABOUT still not working. almost

--- cledesoul/js/app.js
+++ cledesoul/js/app.js
@@ -1,4 +1,16 @@
 console.log(2); //to see if it's reading/working
 
+const aboutContent = document.getElementsByClassName("nav-content");
+console.log(aboutContent);
 
+const aboutBtn = document.getElementsByClassName("about-btn");
+console.log(aboutBtn);
 
+const closeBtn = document.getElementsByClassName("close-btn");
+console.log(closeBtn);
+
+aboutBtn.addEventListener("click", ()=>{
+    console.log("oi");
+    aboutContent.classList.add("show");
+    console.log("oi");
+})
